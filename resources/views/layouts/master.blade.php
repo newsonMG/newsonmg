c8cfad089af2785d2a54d27b395829c288243ec3
newsonMG <monumanewson@gmail.com>
2020-06-15 17:17:31 -0500

    Configuration de la route et vue welcome, creation des dossiers layouts, pages et includes

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+    <title>NewsonMG - @yield('title')</title>
+</head>
+<body>
+    @include('includes.header')
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
